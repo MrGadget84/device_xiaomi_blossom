b568b8c2dd17fd418cef20b584f90b493433a57c
MrGadget84 <superhyper52@gmail.com>
2026-06-06 12:41:13 +0400

    Update AndroidProducts.mk

--- AndroidProducts.mk
+++ AndroidProducts.mk
@@ -5,12 +5,12 @@
 #
 
 PRODUCT_MAKEFILES := \
-    $(LOCAL_DIR)/lineage_blossom.mk
+    $(LOCAL_DIR)/custom_blossom.mk
 
 COMMON_LUNCH_CHOICES := \
-    lineage_blossom-user \
-    lineage_blossom-userdebug \
-    lineage_blossom-eng
+    custom_blossom-user \
+    custom_blossom-userdebug \
+    custom_blossom-eng
 
 PIXELOS_MAINTAINER := MrGadget84
 PRODUCT_MAINTAINER := MrGadget84

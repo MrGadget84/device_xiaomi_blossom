dda2f2a4d0640e52dffb70d074db4ec097159e6a
MrGadget84 <superhyper52@gmail.com>
2026-06-11 07:29:30 +0400

    Update custom_blossom.mk

--- custom_blossom.mk
+++ custom_blossom.mk
@@ -4,6 +4,10 @@
 # SPDX-License-Identifier: Apache-2.0
 #
 
+# SOONG_CONFIG_NAMESPACES += global_test_disable
+# SOONG_CONFIG_global_test_disable := true
+#SOONG_CONFIG_global_test_disable_mode := all
+
 # Inherit from those products. Most specific first.
 $(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
 $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
@@ -13,21 +17,22 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/non_ab_device.mk)
 $(call inherit-product, device/xiaomi/blossom/device.mk)
 
 # Inherit PixelOS stuff
-$(call inherit-product, vendor/pixel/config/common_full_phone.mk)
+$(call inherit-product, vendor/custom/config/common_full_phone.mk)
 
+# Device Options
 scr_resolution := 720
 TARGET_SUPPORTS_BLUR := true
 TARGET_ENABLE_BLUR := true
-
 PRODUCT_NAME := custom_blossom
 PRODUCT_DEVICE := blossom
 PRODUCT_MANUFACTURER := Xiaomi
 PRODUCT_BRAND := Redmi
-
 PRODUCT_GMS_CLIENTID_BASE := android-xiaomi
 
+HALCYON_MAINTAINER := MrGadget84
+PRODUCT_MAINTAINER := MrGadget84
+
+# Addons
 TARGET_FACE_UNLOCK_SUPPORTED := true
 TARGET_DISABLE_MATLOG := true
-
-PIXELOS_MAINTAINER := MrGadget84
-PRODUCT_MAINTAINER := MrGadget84
+TARGET_NO_TESTS := true
